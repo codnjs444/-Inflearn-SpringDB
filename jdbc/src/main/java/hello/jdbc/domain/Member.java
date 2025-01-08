5cdead31dda472732a41968867820c91ecd630aa
codnjs444 <81910499+codnjs444@users.noreply.github.com>
2025-01-08 10:40:45 +0900

    25.01.08 : JDBC 기본 Select

--- jdbc/src/main/java/hello/jdbc/domain/Member.java
+++ jdbc/src/main/java/hello/jdbc/domain/Member.java
@@ -1,21 +1,19 @@
 package hello.jdbc.domain;
 
 import lombok.Data;
-import lombok.extern.slf4j.Slf4j;
 
-@Slf4j
 @Data
 public class Member {
 
     private String memberId;
     private int money;
 
-    public Member(String memberId) {
-        this.memberId = memberId;
+    public Member() {
     }
 
     public Member(String memberId, int money) {
         this.memberId = memberId;
         this.money = money;
     }
+
 }
--- jdbc/src/main/java/hello/jdbc/repository/MemberRepositoryV0.java
+++ jdbc/src/main/java/hello/jdbc/repository/MemberRepositoryV0.java
@@ -5,6 +5,7 @@ import hello.jdbc.domain.Member;
 import lombok.extern.slf4j.Slf4j;
 
 import java.sql.*;
+import java.util.NoSuchElementException;
 
 /**
  * JDBC - DriverManager 사용
@@ -13,7 +14,7 @@ import java.sql.*;
 public class MemberRepositoryV0 {
 
     public Member save(Member member) throws SQLException {
-        String sql = "insert into member(member_id, money) values (?,?)";
+        String sql = "insert into member(member_id, money) values (?, ?)";
 
         Connection con = null;
         PreparedStatement pstmt = null;
@@ -26,11 +27,41 @@ public class MemberRepositoryV0 {
             pstmt.executeUpdate();
             return member;
         } catch (SQLException e) {
-            log.info("db error", e);
-            e.printStackTrace();
+            log.error("db error", e);
             throw e;
         } finally {
             close(con, pstmt, null);
+        }
+
+    }
+
+    public Member findById(String memberId) throws SQLException {
+        String sql = "select * from member where member_id = ?";
+
+        Connection con = null;
+        PreparedStatement pstmt = null;
+        ResultSet rs = null;
+
+        try {
+            con = getConnection();
+            pstmt = con.prepareStatement(sql);
+            pstmt.setString(1, memberId);
+
+            rs = pstmt.executeQuery();
+            if (rs.next()) {
+                Member member = new Member();
+                member.setMemberId(rs.getString("member_id"));
+                member.setMoney(rs.getInt("money"));
+                return member;
+            } else {
+                throw new NoSuchElementException("member not found memberId=" + memberId);
+            }
+
+        } catch (SQLException e) {
+            log.error("db error", e);
+            throw e;
+        } finally {
+            close(con, pstmt, rs);
         }
 
     }
