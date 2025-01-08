f3c291375447c914d2d64738a51e5af92c39df50
codnjs444 <81910499+codnjs444@users.noreply.github.com>
2025-01-08 10:10:07 +0900

    25.01.08 : JDBC 기본 등록

---
+++ jdbc/src/main/java/hello/jdbc/domain/Member.java
@@ -0,0 +1,21 @@
+package hello.jdbc.domain;
+
+import lombok.Data;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Data
+public class Member {
+
+    private String memberId;
+    private int money;
+
+    public Member(String memberId) {
+        this.memberId = memberId;
+    }
+
+    public Member(String memberId, int money) {
+        this.memberId = memberId;
+        this.money = money;
+    }
+}
