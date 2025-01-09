aeb2912f7a89da08a390de3fd5248e608ef2631d
codnjs444 <81910499+codnjs444@users.noreply.github.com>
2025-01-09 09:35:04 +0900

    25.01.09 : Spring Transaction Manager 사용 1단계

--- jdbc/src/main/java/hello/jdbc/repository/MemberRepositoryV3.java
+++ jdbc/src/main/java/hello/jdbc/repository/MemberRepositoryV3.java
@@ -11,7 +11,7 @@ import java.util.NoSuchElementException;
 
 /**
  * 트랜잭션 - 트랜잭션 매니저
- * DataSourceUtils.getConnetion()
+ * DataSourceUtils.getConnection()
  * DataSourceUtils.releaseConnection()
  */
 @Slf4j
@@ -76,37 +76,6 @@ public class MemberRepositoryV3 {
 
     }
 
-    public Member findById(Connection con, String memberId) throws SQLException {
-        String sql = "select * from member where member_id = ?";
-
-        PreparedStatement pstmt = null;
-        ResultSet rs = null;
-
-        try {
-            pstmt = con.prepareStatement(sql);
-            pstmt.setString(1, memberId);
-
-            rs = pstmt.executeQuery();
-            if (rs.next()) {
-                Member member = new Member();
-                member.setMemberId(rs.getString("member_id"));
-                member.setMoney(rs.getInt("money"));
-                return member;
-            } else {
-                throw new NoSuchElementException("member not found memberId=" + memberId);
-            }
-
-        } catch (SQLException e) {
-            log.error("db error", e);
-            throw e;
-        } finally {
-            //connection은 여기서 닫지 않는다.
-            JdbcUtils.closeResultSet(rs);
-            JdbcUtils.closeStatement(pstmt);
-        }
-
-    }
-
     public void update(String memberId, int money) throws SQLException {
         String sql = "update member set money=? where member_id=?";
 
@@ -125,27 +94,6 @@ public class MemberRepositoryV3 {
             throw e;
         } finally {
             close(con, pstmt, null);
-        }
-
-    }
-
-    public void update(Connection con, String memberId, int money) throws SQLException {
-        String sql = "update member set money=? where member_id=?";
-
-        PreparedStatement pstmt = null;
-
-        try {
-            pstmt = con.prepareStatement(sql);
-            pstmt.setInt(1, money);
-            pstmt.setString(2, memberId);
-            int resultSize = pstmt.executeUpdate();
-            log.info("resultSize={}", resultSize);
-        } catch (SQLException e) {
-            log.error("db error", e);
-            throw e;
-        } finally {
-            //connection은 여기서 닫지 않는다.
-            JdbcUtils.closeStatement(pstmt);
         }
 
     }
@@ -173,11 +121,13 @@ public class MemberRepositoryV3 {
     private void close(Connection con, Statement stmt, ResultSet rs) {
         JdbcUtils.closeResultSet(rs);
         JdbcUtils.closeStatement(stmt);
+        //주의! 트랜잭션 동기화를 사용하려면 DataSourceUtils를 사용해야 한다.
         DataSourceUtils.releaseConnection(con, dataSource);
     }
 
 
     private Connection getConnection() throws SQLException {
+        //주의! 트랜잭션 동기화를 사용하려면 DataSourceUtils를 사용해야 한다.
         Connection con = DataSourceUtils.getConnection(dataSource);
         log.info("get connection={}, class={}", con, con.getClass());
         return con;
--- jdbc/src/main/java/hello/jdbc/service/MemberServiceV3_1.java
+++ jdbc/src/main/java/hello/jdbc/service/MemberServiceV3_1.java
@@ -2,8 +2,12 @@ package hello.jdbc.service;
 
 import hello.jdbc.domain.Member;
 import hello.jdbc.repository.MemberRepositoryV2;
+import hello.jdbc.repository.MemberRepositoryV3;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.TransactionStatus;
+import org.springframework.transaction.support.DefaultTransactionDefinition;
 
 import javax.sql.DataSource;
 import java.sql.Connection;
@@ -16,32 +20,30 @@ import java.sql.SQLException;
 @RequiredArgsConstructor
 public class MemberServiceV3_1 {
 
-    private final DataSource dataSource;
-    private final MemberRepositoryV2 memberRepository;
+    private final PlatformTransactionManager transactionManager;
+    private final MemberRepositoryV3 memberRepository;
 
     public void accountTransfer(String fromId, String toId, int money) throws SQLException {
-        Connection con = dataSource.getConnection();
-        try {
-            con.setAutoCommit(false);//트랜잭션 시작
-            //비즈니스 로직
-            bizLogic(con, fromId, toId, money);
-            con.commit(); //성공시 커밋
-        } catch (Exception e) {
-            con.rollback(); //실패시 롤백
-            throw new IllegalStateException(e);
-        } finally {
-            release(con);
-        }
 
+        TransactionStatus status = transactionManager.getTransaction(new DefaultTransactionDefinition());
+
+        try {
+            //비즈니스 로직
+            bizLogic(fromId, toId, money);
+            transactionManager.commit(status);
+        } catch (Exception e) {
+            transactionManager.rollback(status);
+            throw new IllegalStateException(e);
+        }
     }
 
-    private void bizLogic(Connection con, String fromId, String toId, int money) throws SQLException {
-        Member fromMember = memberRepository.findById(con, fromId);
-        Member toMember = memberRepository.findById(con, toId);
+    private void bizLogic(String fromId, String toId, int money) throws SQLException {
+        Member fromMember = memberRepository.findById(fromId);
+        Member toMember = memberRepository.findById(toId);
 
-        memberRepository.update(con, fromId, fromMember.getMoney() - money);
+        memberRepository.update(fromId, fromMember.getMoney() - money);
         validation(toMember);
-        memberRepository.update(con, toId, toMember.getMoney() + money);
+        memberRepository.update(toId, toMember.getMoney() + money);
     }
 
     private void validation(Member toMember) {
